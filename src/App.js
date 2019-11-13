27376f6566272f06cda48fc0e1d32b519f9d9589
Matt Klein <mklein246@gmail.com>
2019-11-13 01:42:36 -0800

    ditched react-scroll-to, got basic scroll, reveal set up. Added fade to starfield.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import './App.css';
-import Starfield from './Starfield'
-import styled from 'styled-components';
+import React, {useState, useRef} from "react";
+import "./App.css";
+import Starfield from "./Starfield";
+import styled from "styled-components";
 
-const Container = styled.div`
-    position: relative;
-    height: 100vh;
-    margin: 0;
-    padding: 0;
-`
+import Zoom from 'react-reveal/Zoom'
+
+const AppSC = styled.div`
+  height: 100%;
+  overflow: ${props => props.scroll ? "auto" : "hidden"};
+`;
+
+const ContainerSC = styled.div`
+  position: relative;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background: #333333;
+`;
+
+const NavBarSC = styled.div`
+  position: fixed;
+  height: 50px;
+  width: 100%;
+  background: #004400;
+  z-index: 5;
+`;
 
 function App() {
+  const [hasStarted, setHasStarted] = useState(false);
+  const scrollTarget = useRef(null);
+
+  const startButtonHandler = async ev => {
+    ev.preventDefault();
+    await setHasStarted(true)
+    window.scrollTo({
+      top: scrollTarget.current.offsetTop,
+      behavior: "smooth"
+    })
+  }
+
   return (
-    <div>
-        <Container>
-          <Starfield></Starfield>
-        </Container>
-        <p>wassup</p>
-        <p>wassup</p>
-        <p>wassup</p>
-    </div>
+    <AppSC scroll={hasStarted}>
+      <NavBarSC></NavBarSC>
+      <ContainerSC>
+        <Starfield startButtonHandler = {startButtonHandler}/>
+      </ContainerSC>
+      {
+        hasStarted &&
+        <>
+          <ContainerSC ref={scrollTarget}>
+            <Zoom>
+              <p>wassup</p>
+              <p>wassup</p>
+              <p>wassup</p>
+              <p>wassup</p>
+              <p>wassup</p>
+              <p>wassup</p>
+            </Zoom>
+          </ContainerSC>
+          <ContainerSC>
+            <p>wassup</p>
+          </ContainerSC>
+          <ContainerSC>
+            <p>wassup</p>
+          </ContainerSC>
+        </>
+      }
+    </AppSC>
   );
 }
 
--- src/Starfield.js
+++ src/Starfield.js
@@ -1,175 +1,226 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
 
 const CanvasSC = styled.canvas`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0%;
-    top: 0%;
-    background: black;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0%;
+  top: 0%;
+  background: black;
 `;
 
-const CanvasBox = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
-`
+const CanvasBoxSC = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  z-index: 6;
+`;
+
+const StartButtonSC = styled.button`
+  background: rgba(0, 0, 0, 0);
+  padding: 20px;
+  margin-bottom: 10vh;
+  border: 1px solid white;
+  border-radius: 20px;
+  color: white;
+  z-index: 7;
+  user-select: none;
+  transition: background 0.3s;
+
+  :hover {
+    background: rgba(50, 50, 50, 20);
+    cursor: pointer;
+  }
+
+  :active {
+    background: rgba(30, 30, 30, 20);
+  }
+
+  :focus {
+    outline: none;
+    border: 1px solid #bbbbbb;
+  }
+`;
 
 let state = {
-    animFrame: 0,
-    mousePosition: {x: 0, y: 0}
+  animFrame: 0,
+  mousePosition: { x: 0, y: 0 }
 };
 
 class Particle {
-    constructor(x, y, hue) {
-        this.hue =
-            hue + (-0.5 * this.hueVariance + Math.random() * this.hueVariance);
-        this.currentX = x;
-        this.currentY = y;
+  constructor(x, y, hue) {
+    this.hue =
+      hue + (-0.5 * this.hueVariance + Math.random() * this.hueVariance);
+    this.currentX = x;
+    this.currentY = y;
+  }
+
+  // Set the speed for the particle
+  speed = {
+    x: -0.5 + Math.random() * 1,
+    y: -0.5 + Math.random() * 1
+  };
+
+  // Size the particle
+  radius = 4 + Math.random() * 4;
+
+  // Set color variance
+  hueVariance = 50;
+
+  // Set radius of circle around mouse the particle will avoid
+  mouseAvoidRadius = 500;
+
+  // Set how quickly the particle should move away from the mouse (higher = faster)
+  mouseAvoidStrength = 5;
+
+  // Function to draw particle on canvas (and update location based on speed)
+  draw = canvas => {
+    console.log("YO");
+    const p = this;
+    const ctx = canvas.getContext("2d");
+
+    // Draw a circle at the current location
+    ctx.beginPath();
+    ctx.arc(p.currentX, p.currentY, p.radius, 0, Math.PI * 2);
+    ctx.fillStyle = `hsl(${p.hue}, 100%, 50%)`;
+    ctx.shadowColor = `hsl(${p.hue}, 100%, 50%)`;
+    ctx.shadowBlur = 25;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(p.currentX, p.currentY, p.radius / 1.5, 0, Math.PI * 2);
+    ctx.fillStyle = `hsl(${p.hue}, 100%, 70%)`;
+    ctx.shadowColor = `hsl(${p.hue}, 100%, 70%)`;
+    ctx.shadowBlur = 10;
+    ctx.fill();
+
+    // Update the particle's location
+    p.currentX += p.speed.x;
+    p.currentY += p.speed.y;
+
+    // adjustment for resolution
+    let mpX = state.mousePosition.x * 4;
+    let mpY = state.mousePosition.y * 4;
+
+    const mouseDistance = Math.sqrt(
+      Math.pow(p.currentX - mpX, 2) + Math.pow(p.currentY - mpY, 2)
+    );
+
+    if (mouseDistance < p.mouseAvoidRadius) {
+      // reset base speed with angle
+
+      let avoidSpeed =
+        p.mouseAvoidStrength *
+        Math.pow((p.mouseAvoidRadius - mouseDistance) / p.mouseAvoidRadius, 2);
+      // also calc currentSpeed ugh
+
+      let moveDistX = ((p.currentX - mpX) * avoidSpeed) / mouseDistance;
+      let moveDistY = ((p.currentY - mpY) * avoidSpeed) / mouseDistance;
+
+      if (state.animFrame % 100) console.log("X", moveDistX, "Y", moveDistY);
+
+      p.currentX += moveDistX;
+      p.currentY += moveDistY;
     }
 
-    // Set the speed for the particle
-    speed = {
-        x: -.5 + Math.random() * 1,
-        y: -.5 + Math.random() * 1
-    };
+    if (p.currentX <= 0) {
+      p.currentX = 0;
+      p.speed.x *= -1;
+    } else if (p.currentX >= canvas.width) {
+      p.currentX = canvas.width;
+      p.speed.x *= -1;
+    }
 
-    // Size the particle
-    radius = 4 + Math.random() * 4;
-
-    // Set color variance
-    hueVariance = 50;
-
-    // Set radius of circle around mouse the particle will avoid
-    mouseAvoidRadius = 500
-
-    // Set how quickly the particle should move away from the mouse (higher = faster)
-    mouseAvoidStrength = 5
-
-    // Function to draw particle on canvas (and update location based on speed)
-    draw = canvas => {
-        console.log("YO")
-        const p = this;
-        const ctx = canvas.getContext("2d");       
-
-        // Draw a circle at the current location
-        ctx.beginPath();
-        ctx.arc(p.currentX, p.currentY, p.radius, 0, Math.PI * 2);
-        ctx.fillStyle = `hsl(${p.hue}, 100%, 50%)`;
-        ctx.shadowColor = `hsl(${p.hue}, 100%, 50%)`;
-        ctx.shadowBlur = 25;
-        ctx.shadowOffsetX = 0;
-        ctx.shadowOffsetY = 0;
-        ctx.fill();
-        ctx.closePath();
-        ctx.beginPath();
-        ctx.arc(p.currentX, p.currentY, p.radius / 1.5, 0, Math.PI * 2);
-        ctx.fillStyle = `hsl(${p.hue}, 100%, 70%)`;
-        ctx.shadowColor = `hsl(${p.hue}, 100%, 70%)`;
-        ctx.shadowBlur = 10;
-        ctx.fill();
-
-        // Update the particle's location
-        p.currentX += p.speed.x;
-        p.currentY += p.speed.y;
-
-        // adjustment for resolution
-        let mpX = state.mousePosition.x * 4 
-        let mpY = state.mousePosition.y * 4
-        
-        const mouseDistance = Math.sqrt(Math.pow(p.currentX - mpX, 2) + Math.pow(p.currentY - mpY, 2))
-
-        if (mouseDistance < p.mouseAvoidRadius) {
-            // reset base speed with angle
-
-            let avoidSpeed = p.mouseAvoidStrength * Math.pow(((p.mouseAvoidRadius - mouseDistance) / p.mouseAvoidRadius), 2)
-            // also calc currentSpeed ugh
-
-            let moveDistX = (p.currentX - mpX) * avoidSpeed / mouseDistance
-            let moveDistY = (p.currentY - mpY) * avoidSpeed / mouseDistance
-
-            if (state.animFrame % 100) console.log("X", moveDistX, "Y", moveDistY)
-
-            p.currentX += moveDistX
-            p.currentY += moveDistY
-
-        }
-
-        if (p.currentX <= 0) {
-            p.currentX = 0;
-            p.speed.x *= -1
-            
-        } else if (p.currentX >= canvas.width) {
-            p.currentX = canvas.width;
-            p.speed.x *= -1
-        }
-        if (p.currentY <= 0) {
-            p.currentY = 0;
-            p.speed.y *= -1
-        } else if (p.currentY >= canvas.height) {
-            p.currentY = canvas.height;
-            p.speed.y *= -1
-        }
-    };
+    if (p.currentY <= 0) {
+      p.currentY = 0;
+      p.speed.y *= -1;
+    } else if (p.currentY >= canvas.height) {
+      p.currentY = canvas.height;
+      p.speed.y *= -1;
+    }
+  };
 }
 
+export default function Starfield(props) {
+  const canvasRef = useRef(null);
+  let totalParticles = 40;
+  let particles = [];
 
+  let [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
 
-export default function Starfield() {
-    const canvasRef = useRef(null);
-    let totalParticles = 40;
-    let particles = [];
+  useEffect(() => {
+    setDimensions({ width: window.innerWidth, height: window.innerHeight });
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
-    let [dimensions, setDimensions] = useState({width: window.innerWidth, height: window.innerHeight})
+    window.addEventListener("resize", () => {
+      setDimensions({ width: window.innerWidth, height: window.innerHeight });
+      particles = [];
+      ctx.globalAlpha = 0;
+      startAnimateParticles(canvas);
+    });
 
-    useEffect(() => {
-        setDimensions({width: window.innerWidth, height: window.innerHeight})
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext("2d")
+    ctx.globalAlpha = 0;
+    startAnimateParticles(canvas);
+    animateParticles();
 
-        window.addEventListener('resize', () => {
-            setDimensions({width: window.innerWidth, height: window.innerHeight})
-            particles = []
-            startAnimateParticles(canvas)
-        });
+    function startAnimateParticles(canvas) {
+      for (let j = 0; j < totalParticles; j++) {
+        particles = particleEmitter(particles);
+      }
 
-        startAnimateParticles(canvas)
-        animateParticles();
+      function particleEmitter(particles) {
+        let hue = 200;
+        let x = Math.floor(canvas.width * Math.random());
+        let y = Math.floor(canvas.height * Math.random());
 
-        function startAnimateParticles(canvas) {
-            for (let j = 0; j < totalParticles; j++) {
-                particles = particleEmitter(particles);
-            }
-        
-            function particleEmitter(particles) {
-                let hue = 200;
-                let x = Math.floor(canvas.width * Math.random());
-                let y = Math.floor(canvas.height * Math.random());
-        
-                return [...particles, new Particle(x, y, hue)];
-            }
-        }
-
-        function animateParticles() {
-            state = {...state, animFrame: requestAnimationFrame(animateParticles)};
-            // Clear out the old particles
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-            // Draw all of our particles in their new location
-            for (let i = 0; i < particles.length; i++) {
-                particles[i].draw(canvas);
-            }
-        }
-    }, []);
-
-    function setMousePosition(ev) {
-        state = {...state, mousePosition: {x: ev.nativeEvent.offsetX, y: ev.nativeEvent.offsetY}}
+        return [...particles, new Particle(x, y, hue)];
+      }
     }
 
-    return (
-        <CanvasBox>
-            <CanvasSC width={dimensions.width * 4} height={dimensions.height * 4} onMouseMove={setMousePosition} ref={canvasRef} />
-        </CanvasBox>
-    );
+    function animateParticles() {
+      state = { ...state, animFrame: requestAnimationFrame(animateParticles) };
+      // Clear out the old particles
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      if (ctx.globalAlpha < 1) {
+        ctx.globalAlpha += .002
+      }
+
+      // Draw all of our particles in their new location
+      for (let i = 0; i < particles.length; i++) {
+        particles[i].draw(canvas);
+      }
+    }
+
+    return () => window.cancelAnimationFrame(state.animFrame)
+  }, []);
+
+  function setMousePosition(ev) {
+    state = {
+      ...state,
+      mousePosition: { x: ev.nativeEvent.offsetX, y: ev.nativeEvent.offsetY }
+    };
+  }
+
+  return (
+    <CanvasBoxSC>
+      <StartButtonSC onClick={props.startButtonHandler}>PRESS ME</StartButtonSC>
+      <CanvasSC
+        width={dimensions.width * 4}
+        height={dimensions.height * 4}
+        onMouseMove={setMousePosition}
+        ref={canvasRef}
+      />
+    </CanvasBoxSC>
+  );
 }
