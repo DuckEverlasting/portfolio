f61c051802b4239abaf56c8f59b478ceaaeab65e
Matt Klein <mklein246@gmail.com>
2019-12-04 15:08:39 -0800

    pretty happy with the unfold component!

--- src/components/ContactUnfold.js
+++ src/components/ContactUnfold.js
@@ -9,37 +9,42 @@ function ContactUnfold({ isOn }) {
         : "rotateX(-0.5turn)",
     config: 
       isOn
-        ? { mass: 1, tension: 40, friction: 16 }
-        : { mass: 1, tension: 18, friction: 16 },
-    delay: isOn ? 0 : 1500
+        ? { mass: 1, tension: 40, friction: 12 }
+        : { mass: 1, tension: 38, friction: 16 },
+    delay: isOn ? 0 : 600
   })
   const unfoldSpr2 = useSpring({
     transform:
       isOn
         ? "rotateX(0turn)"
-        : "rotateX(-0.4turn)",
+        : "rotateX(-0.25turn)",
     config: 
       isOn
-        ? { mass: 1, tension: 40, friction: 14 }
-        : { mass: 1, tension: 18, friction: 14 },
-    delay: isOn ? 125 : 1000
+        ? { mass: 1, tension: 40, friction: 12 }
+        : { mass: 1, tension: 38, friction: 14 },
+    delay: isOn ? 125 : 400
   })
   const unfoldSpr3 = useSpring({
     transform:
       isOn
         ? "rotateX(0turn)"
-        : "rotateX(-0.4turn)",
+        : "rotateX(-0.25turn)",
     config: 
       isOn
-        ? { mass: 1, tension: 40, friction: 12 }
-        : { mass: 1, tension: 18, friction: 12 },
-    delay: isOn ? 250 : 500
+        ? { mass: 1, tension: 40, friction: 14 }
+        : { mass: 1, tension: 38, friction: 12 },
+    delay: isOn ? 250 : 200
   })
   const unfoldSpr4 = useSpring({
+    // CHANGE HEIGHT HERE
     transform:
       isOn
         ? "rotateX(0turn)"
         : "rotateX(-0.5turn)",
+    // maxHeight:
+    //   isOn
+    //     ? "14vh"
+    //     : "8vh",
     config: 
       isOn
         ? { mass: 1, tension: 40, friction: 10 }
@@ -50,21 +55,24 @@ function ContactUnfold({ isOn }) {
   return (
     <div className="contact-unfold-container">
       <animated.div className="contact-unfold-box first-box" style={unfoldSpr1}>
-        <div className="contact-unfold-front first" style={{background: "red"}}/>
+        <div className="contact-unfold-front first"/>
         <div className="contact-unfold-back first"/>
-        <p className="fold-label first">ONE</p>
+        <div className="fold-content first">
+        </div>
         <animated.div className="contact-unfold-box" style={unfoldSpr2}>
-          <div className="contact-unfold-front second" style={{background: "blue"}}/>
+          <div className="contact-unfold-front second"/>
           <div className="contact-unfold-back second"/>
-          <p className="fold-label second">TWO</p>
+          <div className="fold-content second">(Social media icons here)</div>
           <animated.div className="contact-unfold-box" style={unfoldSpr3}>
-            <div className="contact-unfold-front third" style={{background: "green"}}/>
+            <div className="contact-unfold-front third"/>
             <div className="contact-unfold-back third"/>
-            <p className="fold-label third">THREE</p>
+            <div className="fold-content third">
+              <button>SEND ME AN EMAIL</button>
+            </div>
             <animated.div className="contact-unfold-box" style={unfoldSpr4}>
-              <div className="contact-unfold-front fourth" style={{background: "orange"}}/>
+              <div className="contact-unfold-front fourth"/>
               <div className="contact-unfold-back fourth"/>
-              <p className="fold-label fourth">FOUR</p>
+              <div className="fold-content fourth">Have questions or comments? I'd love to hear them!</div>
             </animated.div>
           </animated.div>
         </animated.div>
