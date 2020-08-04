9a44c001fd14405d7d8421639c0eade91a035246
Matt Klein <mklein246@gmail.com>
2020-08-04 14:05:37 -0700

    organized sass pages, changed About content

--- src/components/AboutTextBox.js
+++ src/components/AboutTextBox.js
@@ -23,10 +23,11 @@ function AboutTextBox({ isOn }) {
       {
         <animated.div className="text-box" style={{...textBoxWidthSpr, ...textBoxHeightSpr}}>
           <p className="about-text">
-            I am a full stack web developer based in Los Angeles, CA. 
-            I enjoy making things that work, and fixing things that don't. 
-            My background is in film and television production; I've been an editor, a graphic designer, and a colorist. 
-            Now I'm looking for the next challenge. 
+            I am a full stack web developer specializing in interactive design and image manipulation. 
+            I have a background is in film production; I've been 
+            an editor, a graphic designer, and a colorist. 
+            Now I'm looking to take that experience and apply it to designing and creating 
+            beautiful, functional websites.
           </p>
         </animated.div>
       }
