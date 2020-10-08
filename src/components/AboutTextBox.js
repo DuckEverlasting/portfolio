0e8cd1a7563a96aa0be59310565c1991b80ed042
Matt Klein <mklein246@gmail.com>
2020-10-08 09:39:31 -0700

    removed a word

--- src/components/AboutTextBox.js
+++ src/components/AboutTextBox.js
@@ -24,7 +24,7 @@ function AboutTextBox({ isOn, isLoaded }) {
         <animated.div className="text-box" style={{...textBoxWidthSpr, ...textBoxHeightSpr}}>
           <p className="about-text">
             I am a full stack web developer specializing in interactive design and image manipulation. 
-            I have a background is in film production; I've been 
+            I have a background in film production; I've been 
             an editor, a graphic designer, and a colorist. 
             Now I'm looking to take that experience and apply it to designing and creating 
             beautiful, functional websites.
